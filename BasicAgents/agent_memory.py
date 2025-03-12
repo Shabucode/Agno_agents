f609f5e941f9193c0394e16828c289524a5e7404
Shabucode <shabnamb4u@gmail.com>
2025-03-12 13:33:06 +0000

    Creating agent memory

---
+++ BasicAgents/agent_memory.py
@@ -0,0 +1,6 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.embedder.openai import OpenAIEmbedder
+from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.knowledge.pdf_url import PDFUrlKnowledgeBase
+from agno.vectordb.lancedb import LanceDb, SearchType
